e9aff973a56b5fe09ae109a38ea953022f463866
AbelIbarraSol <abelibarrasol18@gmail.com>
2025-04-05 22:59:34 -0500

    Creacion del proyecto

---
+++ src/Pelicula.java
@@ -0,0 +1,6 @@
+public class Pelicula {
+    String nombre, pais, direccion, genero;
+    int lanzamiento, duracion;
+    boolean incluidoEnElPlan;
+    double clasificacion;
+}
---
+++ src/Principal.java
@@ -0,0 +1,14 @@
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula pelicula = new Pelicula();
+
+        pelicula.direccion = "Gints Zilbalodis";
+        pelicula.nombre = "Flow";
+        pelicula.pais = "Letonia";
+        pelicula.clasificacion = 7.9;
+        pelicula.genero = "Animación";
+        pelicula.duracion = 80;
+        pelicula.lanzamiento = 2024;
+        pelicula.incluidoEnElPlan = true;
+    }
+}
