23ff6f958ba557a89a8042cec3f35342ded1e7db
AbelIbarraSol <abelibarrasol18@gmail.com>
2025-04-16 17:39:10 -0500

    Implementacion de metodo getClasificacion de interface

--- src/com/strangecorp/screenmatch/models/Pelicula.java
+++ src/com/strangecorp/screenmatch/models/Pelicula.java
@@ -1,8 +1,15 @@
 package com.strangecorp.screenmatch.models;
 
-public class Pelicula extends Titulo{
+import com.strangecorp.screenmatch.calculations.Clasificacion;
+
+public class Pelicula extends Titulo implements Clasificacion {
 
     public Pelicula(String nombre, String genero, String director, String pais, String idioma, String sinopsis, int lanzamiento, int duracion, boolean incluidoEnElPlan, boolean postCreditos) {
         super(nombre, genero, director, pais, idioma, sinopsis, lanzamiento, duracion, incluidoEnElPlan, postCreditos);
+    }
+
+    @Override
+    public int getClasificacion() {
+        return (int) (calculoDeMedia() / 2);
     }
 }
