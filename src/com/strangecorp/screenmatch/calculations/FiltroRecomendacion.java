2fd18fe24071a34f9fe76795f2112245531b2087
AbelIbarraSol <abelibarrasol18@gmail.com>
2025-04-16 17:22:18 -0500

    Condicionales para filtro segun calificacion

--- src/com/strangecorp/screenmatch/calculations/FiltroRecomendacion.java
+++ src/com/strangecorp/screenmatch/calculations/FiltroRecomendacion.java
@@ -1,4 +1,16 @@
 package com.strangecorp.screenmatch.calculations;
 
 public class FiltroRecomendacion {
+
+    public void filtro (Clasificacion clasificacion) {
+        if (clasificacion.getClasificacion() >= 4) {
+            System.out.println("Aclamado por la critica");
+        } else if (clasificacion.getClasificacion() >= 2) {
+            System.out.println("Popular entre la critica");
+        } else {
+            System.out.println("Agregalo a tu lista para poder verlo luego");
+        }
+
+    }
+
 }
